8cd5481bf7a11058832598694c23eba1c7083c7b
Çağatay <osman@kayi.oba.org>
2019-10-16 17:11:22 +0300

    revisions

--- dosya.py
+++ dosya.py
@@ -102,6 +102,27 @@ def provisionalListWithHtmlExt(path,extension):
         provisionalHtmlFiles.append(provisionalHtmlFile)
     os.chdir(pwd)
     return provisionalHtmlFiles   
+
+class Tekildosya:
+    def __init__(self,fileName):
+        self.fileStrings=fileName.split(".")
+        self.SecondNameFlag=False
+        self.fileExtension=False
+
+        if len(self.fileStrings)==3:
+            self.SecondNameFlag=True
+            self.fileExtension=True
+            self.fileFirstName=self.fileStrings[0]
+            self.fileSecondName=self.fileStrings[1]
+            self.fileExtension=self.fileStrings[2]
+        if len(self.fileStrings)==2:
+            self.fileExtension=True
+            self.fileFirstName=self.fileStrings[0]
+            self.fileExtension=self.fileStrings[1]
+        if len(self.fileStrings)==1:
+            self.fileFirstName=self.fileStrings[0]
+        print (self.fileStrings)
+        print (len(self.fileStrings))
 if "__main__"==__name__:
     print("Selam")
     f=input("dosya ismi-oku:")
